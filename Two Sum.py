9f3ff6d3b3d9655e12c40f60bc55b6dbd4d8d8a5
Avijit Roy <aviroy2005@gmail.com>
2025-03-31 22:28:10 +0600

    p2-add_two_num

--- Two Sum.py
+++ Two Sum.py
@@ -1,1 +1,15 @@
-print("Hello World")
+#p1 - Two Sum
+
+nums = [2,7,11,15]
+target = 17
+
+class Solution(object):
+    def twoSum( self, nums, target):
+        hash_map = {}
+
+        for i, num in enumerate(nums):
+            if target - num in hash_map:
+                return [hash_map[target - num], i]
+            hash_map[num] = i
+        return []
+    print(twoSum(nums,target))
